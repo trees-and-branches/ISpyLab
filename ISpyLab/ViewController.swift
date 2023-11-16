3b4cbca8e35e5ac37932feba98b0e79817a83d12
spoons <ev4n.mus1c@gmail.com>
2023-11-16 08:50:04 -0700

    finished

--- ISpyLab/ViewController.swift
+++ ISpyLab/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var friesImage: UIImageView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return friesImage
+    }
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / friesImage.bounds.width
+        let heightScale = size.height / friesImage.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
 
+    }
+    
 
 }
 
